88bdb5722ea57caff33252922821c8b9bb1bb337
xxxDENxx <62527798+xxxDENxx@users.noreply.github.com>
2020-03-23 14:46:26 +1000

    Add files via upload

---
+++ The First Lab.py
@@ -0,0 +1,31 @@
+flag = True
+while flag:
+    val_1 = int(input('Input first number: '))
+    val_2 = int(input('Input second number: '))
+    command = input('input operation: ')
+    if command == '+':
+        print(val_1 + val_2)
+    elif command == '-':
+        print(val_1 - val_2)
+    elif command == '*':
+        print(val_1 * val_2)
+    elif command == '/':
+        print(val_1 / val_2)
+    else:
+        print('Wrong command')
+    for i in range(3):
+        command = input('Continue?(Y/N)')
+        if command == 'Y':
+            break
+        elif command == "N":
+            flag = False
+            break
+        else:
+            print('wrong command')
+        if i == 2:
+            print('Too much errors')
+            flag = False
+            break
+
+
+
